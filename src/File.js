3904b588822bd2f9f2d1d5fff40c8b526cbe4365
Craig Spence <craigspence0@gmail.com>
2015-03-20 15:58:13 +1300

    sweet haxors.

--- src/File.js
+++ src/File.js
@@ -450,7 +450,7 @@ File.prototype.chmod = function(callback) {
 File.prototype.chown = function(callback) {
   var self = this;
 
-  if ('owner' in this || 'group' in this) {
+  if (os.platform() !== 'win32' && ('owner' in this || 'group' in this)) {
     uidNumber('owner' in this ? this.owner : process.getuid(), 'group' in this ? this.group : process.getgid(), function(err, uid, gid) {
       if (err) callback(err);
       FS.chown(self.path, uid, gid, function(err) {
